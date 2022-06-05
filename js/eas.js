af46da647098f4fd8f0281363db2ac4b3fee94c9
jpoe <16984557+jpoehl@users.noreply.github.com>
2022-06-05 11:24:26 +0200

    Add 'etching' function

--- js/eas.js
+++ js/eas.js
@@ -2,15 +2,19 @@
 const sketchpad = document.getElementById("sketchpad");
 
 // Functions
-
 function makeGrid(gridContainer, n) {
     gridContainer.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
 
     for (let i = 1; i <= n * n; i++) {
         const slate = document.createElement("div");
         slate.classList.add("slate");
+        slate.addEventListener("mouseover", etch);
         gridContainer.appendChild(slate);
     }
+}
+
+function etch(e, color = "black") {
+    e.target.style.backgroundColor = color;
 }
 
 // Function calls
