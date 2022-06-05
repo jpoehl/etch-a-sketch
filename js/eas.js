31eb261a37effdf5289d607ecfc446673d7d7d65
jpoe <16984557+jpoehl@users.noreply.github.com>
2022-06-05 12:57:40 +0200

    Implement toggle between black and random color

--- js/eas.js
+++ js/eas.js
@@ -10,13 +10,10 @@ function makeGrid(gridContainer, n) {
     }
 }
 
-function etch(e) {
-    e.target.classList.add("black");
-}
-
 function erase(e) {
     const slates = document.querySelectorAll(".slate");
-    slates.forEach(s => s.className = "slate");
+    slates.forEach(s => s.style.backgroundColor = "white");
+    etchColor = "black";
 }
 
 function getRandomColor() {
@@ -29,14 +26,29 @@ function getRandomColor() {
     return color;
 }
 
+function etch(e) {
+    let selectedColor;
+
+    if (etchColor == "black") {
+        selectedColor = "black";
+    } else {
+        selectedColor = getRandomColor();
+    }
+
+    e.target.style.backgroundColor = selectedColor;
+}
+
 // Variables
 const sketchpad = document.getElementById("sketchpad");
+let etchColor = "black";
+
+document.getElementById("randomBtn").addEventListener("click", () => etchColor = "random");
+document.getElementById("blackBtn").addEventListener("click", () => etchColor = "black");
 
 // Buttons
 //* Erase
 const eraseBtn = document.getElementById("eraser");
 eraseBtn.addEventListener("click", erase);
-
 
 // Function calls
 makeGrid(sketchpad, 16);
